fadd9bde823f4a661fd959582e8fb6900c0e7e48
Đỗ Tuấn Anh <65461336+Nino610@users.noreply.github.com>
2021-06-12 01:04:04 +0700

    DoneGiaoDienLogin

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
-
+  constructor(private fb: FormBuilder) {}
+  myForm: FormGroup;
   ngOnInit(): void {}
 }
