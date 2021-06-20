bba686e44ddc2bbeebb7290901eb0c2948375dce
Đỗ Tuấn Anh <65461336+Nino610@users.noreply.github.com>
2021-06-21 00:07:25 +0700

    DoneExportAndSearch

--- src/app/main/admin/employees/employees.component.ts
+++ src/app/main/admin/employees/employees.component.ts
@@ -25,6 +25,9 @@ export class EmployeesComponent implements OnInit {
   basicOptions: any;
   displayBasic: boolean = false;
   displayBasicUpdate: boolean = false;
+  statuses: any[];
+  loading: boolean = true;
+  activityValues: number[] = [0, 100];
   constructor(
     public service: EmployeeService,
     public managersv: ManagerService,
@@ -34,6 +37,32 @@ export class EmployeesComponent implements OnInit {
 
   showBasicDialog() {
     this.displayBasic = true;
+  }
+  exportExcel() {
+    import('xlsx').then((xlsx) => {
+      const worksheet = xlsx.utils.json_to_sheet(this.listEmployee);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+      const excelBuffer: any = xlsx.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      });
+      this.saveAsExcelFile(excelBuffer, 'products');
+    });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    import('file-saver').then((FileSaver) => {
+      let EXCEL_TYPE =
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+      let EXCEL_EXTENSION = '.xlsx';
+      const data: Blob = new Blob([buffer], {
+        type: EXCEL_TYPE,
+      });
+      FileSaver.saveAs(
+        data,
+        fileName + 'export' + new Date().getTime() + EXCEL_EXTENSION
+      );
+    });
   }
   logout() {
     localStorage.removeItem('token');
